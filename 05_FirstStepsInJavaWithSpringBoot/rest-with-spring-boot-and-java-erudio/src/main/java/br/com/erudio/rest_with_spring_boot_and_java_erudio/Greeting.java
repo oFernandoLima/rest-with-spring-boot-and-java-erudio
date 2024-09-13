31ac3e231dc4b6357c2cf3a2110590f3b5fce9b8
Fernando Lima <eufernandoprogramador@gmail.com>
2024-09-13 04:23:18 -0300

    First steps in Java with Spring Boot

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/rest_with_spring_boot_and_java_erudio/Greeting.java
@@ -0,0 +1,21 @@
+package br.com.erudio.rest_with_spring_boot_and_java_erudio;
+
+public class Greeting {
+    
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+   
+}
